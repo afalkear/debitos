51fe357d3f542ad521d556f9b71a210e1850647b
Alex Falke <afalkear@gmail.com>
2012-11-25 17:54:55 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Debitos::Application.config.secret_token = 'a9b8343e405123563f65ea05a2aeab91455a07e586c99d51d8ae7870c9c1e8c33886bc22042460edc98f12edb6f240d300e8dcb19d9bf753b5d8a3741841bd97'
