01d1b26a278caaebaa6f537626419988be3a12ef
Alex Falke <afalkear@gmail.com>
2013-01-11 09:33:30 -0300

    minor changes

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -38,7 +38,7 @@ class SummariesController < ApplicationController
 
 
     total_count = alumnos.count.to_s.rjust(7, "0")
-    total_amount = sprintf("%.02f", alumnos.sum("amount").to_s).rjust(15, "0")
+    total_amount = sprintf("%.02f", alumnos.sum("amount").to_s).rjust(16, "0").delete(".")
     # filename = DEBLIQ + "C" if params[:type => "credito"] algo así
     first_line = begin_register_type+constant_type+"C"+" "+establishment.rjust(10, "0")+constant+
                   date+hour+debit_type+status+begin_reserved+mark
@@ -49,7 +49,7 @@ class SummariesController < ApplicationController
       f.puts(first_line)
       alumnos.each do |alumno|
         alumno_line = alumno_register_type+alumno.card_number.to_s.rjust(16, "0")+alumno_reserved_space_1+alumno.bill.to_s.rjust(8, "0")+
-                      date+transaction_code+sprintf("%.02f", alumno.amount.to_s).rjust(15, "0")+alumno.identifier.to_s.rjust(15, "0")+
+                      date+transaction_code+sprintf("%.02f", alumno.amount.to_s).rjust(16, "0").delete(".")+alumno.identifier.to_s.rjust(15, "0")+
                       (alumno.new_debit? ? "E" : " ")+"  "+alumno_reserved_space_2+mark
         f.puts(alumno_line)
       end
